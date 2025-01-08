8d6fc76c8e74ce95fa19fac6c71d4fefd3db9211
zabronax <zabronax@gmail.com>
2025-01-08 14:42:59 +0100

    setup resend client

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,19 @@
+import { Resend } from "resend"
+
+const apiKey = process.env.RESEND_API_KEY
+const resend = new Resend(apiKey)
+
 export function GET() {
+
+  resend.emails.send({
+    from: "contact@mypage.no",
+    to: "Larsien973spam@gmail.com",
+    subject: "Contact wanted message",
+    html: `
+      <h1>Message from somebody</h1>
+      <p>They said nice things about me</p>
+    `,
+  })
+
   return new Response("Hello World!")
 }
