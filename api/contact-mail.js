46fd2652efe5120d07b6fb6c76f52148be10b124
zabronax <zabronax@gmail.com>
2025-01-09 09:53:36 +0100

    change to post endpoint

---
+++ api/contact-mail.js
@@ -0,0 +1,24 @@
+import { Resend } from "resend";
+
+const apiKey = process.env.RESEND_API_KEY;
+const resend = new Resend(apiKey);
+
+export async function POST(request) {
+  console.log(request);
+
+  // console.log("Starting sending mail via resend");
+
+  // const result = await resend.emails.send({
+  //   from: "Lars <contact@resend.dev>",
+  //   to: "lars.gunnar@jobloop.no",
+  //   subject: "Contact wanted message",
+  //   html: `
+  //     <h1>Message from somebody</h1>
+  //     <p>They said nice things about me</p>
+  //   `,
+  // });
+
+  // console.log(result.error.message);
+
+  return new Response("Hello World!");
+}
