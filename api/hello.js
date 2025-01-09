9855671b3458f73c4a10eecc6b2571852dbd8dad
zabronax <zabronax@gmail.com>
2025-01-09 09:43:00 +0100

    add logging to resend

--- api/hello.js
+++ api/hello.js
@@ -1,11 +1,12 @@
-import { Resend } from "resend"
+import { Resend } from "resend";
 
-const apiKey = process.env.RESEND_API_KEY
-const resend = new Resend(apiKey)
+const apiKey = process.env.RESEND_API_KEY;
+const resend = new Resend(apiKey);
 
-export function GET() {
+export async function GET() {
+  console.log("Starting sending mail via resend");
 
-  resend.emails.send({
+  const result = await resend.emails.send({
     from: "Lars <contact@resend.dev>",
     to: "lars.gunnar@jobloop.no",
     subject: "Contact wanted message",
@@ -13,7 +14,9 @@ export function GET() {
       <h1>Message from somebody</h1>
       <p>They said nice things about me</p>
     `,
-  })
+  });
 
-  return new Response("Hello World!")
+  console.log(result.error.message);
+
+  return new Response("Hello World!");
 }
