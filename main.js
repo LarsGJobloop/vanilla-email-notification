4d883a0afaeda669558bf195daa50693fcead5c1
zabronax <zabronax@gmail.com>
2025-01-09 10:43:17 +0100

    add contact form handler

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const contactMessage = {
-  email: "kevin@home.alone",
-  message: "Freeze M****ers!",
-};
+const formElement = document.querySelector("#contact-form");
+if (formElement === null) {
+  throw Error("Hey! You are missing the form element with id: contact-form");
+}
 
-const response = await fetch("/api/contact-mail", {
-  method: "POST",
-  body: JSON.stringify(contactMessage),
+formElement.addEventListener("submit", async (event) => {
+  // Prevent form submission from refreshing page
+  event.preventDefault();
+
+  // Find the values of the input fields
+  const contactMessage = {
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+  };
+
+  // Send message to the backend (serverless function)
+  const response = await fetch("/api/contact-mail", {
+    method: "POST",
+    body: JSON.stringify(contactMessage),
+  });
+
+  // If success clear input fields
+  if (response.ok) {
+    formElement.reset();
+  }
 });
-const data = await response.text();
-console.log(data);
