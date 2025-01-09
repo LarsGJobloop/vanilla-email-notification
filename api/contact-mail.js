9c1f825dd8080b8decff95658e8562727451a24a
zabronax <zabronax@gmail.com>
2025-01-09 10:54:51 +0100

    setup mail handler

--- api/contact-mail.js
+++ api/contact-mail.js
@@ -4,22 +4,29 @@ const apiKey = process.env.RESEND_API_KEY;
 const resend = new Resend(apiKey);
 
 export async function POST(request) {
-  const data = await request.json();
-  console.log(data);
+  const contactFormData = await request.json();
 
-  // console.log("Starting sending mail via resend");
+  const result = await resend.emails.send({
+    from: "Lars <contact@resend.dev>",
+    to: "lars.gunnar@jobloop.no",
+    replyTo: contactFormData.email,
+    subject: "Contact wanted message",
+    html: `
+      <h1>Message from somebody</h1>
+      <h2>${contactFormData.email}</h2>
+      <p>${contactFormData.message}</p>
+    `,
+  });
 
-  // const result = await resend.emails.send({
-  //   from: "Lars <contact@resend.dev>",
-  //   to: "lars.gunnar@jobloop.no",
-  //   subject: "Contact wanted message",
-  //   html: `
-  //     <h1>Message from somebody</h1>
-  //     <p>They said nice things about me</p>
-  //   `,
-  // });
+  if (result.error !== null) {
+    console.log(result.error.name);
+    console.log(result.error.message);
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
 
-  // console.log(result.error.message);
-
-  return new Response("Hello World!");
+  return new Response("Message sent", {
+    status: 200,
+  });
 }
